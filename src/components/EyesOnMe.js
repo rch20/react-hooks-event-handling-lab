68e379d30efeb2b76e60a904b525ce1f51fae383
rch20 <reemshalak22@hotmail.com>
2022-10-17 18:47:49 +0300

    assignment done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+export default function EyesOnMe() {
+  const handleOnFocus = () => {
+    console.log("Good!");
+  };
+
+  const handleOnBlur = () => {
+    console.log("Hey! Eyes on me!");
+  };
+
+  return (
+    <div>
+      <button onFocus={handleOnFocus} onBlur={handleOnBlur}>
+        Eyes on me
+      </button>
+    </div>
+  );
+}
